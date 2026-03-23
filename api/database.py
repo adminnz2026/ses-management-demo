dc21378a08d94fbfd3a7b32360ea2ee0e168ec52
adminnz2026 <flust0zn@gmail.com>
2026-03-23 11:21:56 +0900

    initialize

--- api/database.py
+++ api/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# 接続情報: mysql+pymysql://ユーザー名:パスワード@ホスト:ポート/データベース名
-SQLALCHEMY_DATABASE_URL = "mysql://45WBt8b8nAYyPaa.root:PfZnHLKtUC03ES9D@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/ses_management_demo"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://45WBt8b8nAYyPaa.root:PfZnHLKtUC03ES9D@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/ses_management_demo?ssl_ca=/etc/ssl/certs/ca-certificates.crt"
 
-# エンジンの作成
+# エンジン作成時の引数も TiDB 向けに調整
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,  # 接続切れ対策
-    echo=False           # SQLログを出力したい場合はTrue
+    connect_args={"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}, # VercelなどのLinux環境用
+    pool_pre_ping=True,
+    pool_recycle=300 # TiDB側で接続が切られるのを防ぐため短めに設定
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
